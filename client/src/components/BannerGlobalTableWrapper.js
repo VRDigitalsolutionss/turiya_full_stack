580c29a0ba8a0b0192073e31b18b803718987c60
rishu578803 <rishuam120@gmail.com>
2025-01-01 02:24:15 +0530

    link changed in course

--- client/src/components/BannerGlobalTableWrapper.js
+++ client/src/components/BannerGlobalTableWrapper.js
@@ -258,7 +258,7 @@ const BannerGlobalTableWrapper = () => {
     } else if (location == 'Himalaya Indien') {
       navigate('/yogalehrer-ausbildung-himalaya-indien')
     } else if (location == 'Berlin') {
-      navigate('/YOGALEHRERAUSBILDUNG_BERLIN')
+      navigate('/YOGALEHRERAUSBILDUNG-BERLIN')
     } else{
       alert("Page is under development. Sorry for the inconvenience.")
     }
